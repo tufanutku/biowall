bc0a5b0bd32981046755272e361932b8df96ce75
tufanutku <tufanutk_u@hotmail.com>
2016-11-24 15:19:44 +0200

    Update pubmedSearcherCodes.js

--- pubmedSearcherCodes.js
+++ pubmedSearcherCodes.js
@@ -1,11 +1,20 @@
+var request = require("request");
+var cheerio = require("cheerio");
+
 function search(){
-    console.log('working');
+    console.log('1');
         var obj = document.getElementById('lel');
+        console.log(obj.value);
         var unencoded = obj.value;
         obj.value = encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");    
         $("#link").attr('href', function() {
-        return this.href + unencoded;
+        var finLink = this.href + unencoded;
+        console.log(finLink);
+            // request (finLink, function(error, response, html) {
+            //     if (!error && response.statusCode == 200) {
+            //         var $ = cheerio.load(html);
+            //         console.log('reached');
+            //     }
+            // });
         });
-        console.log($("#link"));
-        location.href=document.getElementById("link");
 }
